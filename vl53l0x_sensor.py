c469dc0a8a16c00ae6a3f01299a3951bfa5fc0ec
Ethan Look <ethan.look@gmail.com>
2023-04-13 16:45:39 +0100

    Convert VL53L0X sensor to a camera to produce point clouds.

--- vl53l0x_sensor.py
+++ vl53l0x_sensor.py
@@ -4,24 +4,43 @@ Python implementation of the VL53L0X Time of Flight sensor for Viam.
 
 import asyncio
 import logging
+import os
 import sys
-from typing import Any, Mapping
+import tempfile
+from typing import Any, Optional, Tuple, Union
+
+from PIL.Image import Image
 
 import adafruit_vl53l0x
 import board
 import busio
-from viam.components.sensor import Sensor
+import numpy as np
+import open3d as o3d
+from viam.components.camera import Camera
+from viam.media.video import RawImage
 from viam.rpc.server import Server
 
 
-class VL53L0X(Sensor):
+class VL53L0X(Camera):
     def __init__(self, name: str):
         i2c = busio.I2C(board.SCL, board.SDA)
         self.vl53 = adafruit_vl53l0x.VL53L0X(i2c)
         super().__init__(name)
 
-    async def get_readings(self, **kwargs) -> Mapping[str, Any]:
-        return {"range": self.vl53.range}
+    async def get_point_cloud(self, *, timeout: Optional[float] = None, **kwargs) -> Tuple[bytes, str]:
+        pc = o3d.geometry.PointCloud()
+        points = np.array([[self.vl53.range / 1000, 0, 0]])
+        pc.points = o3d.utility.Vector3dVector(points)
+        path = os.path.join(tempfile.mkdtemp(), 'temp.pcd')
+        o3d.io.write_point_cloud(path, pc)
+        data = open(path, 'rb').read()
+        return (data, "pointcloud/pcd")
+
+    async def get_image(self, mime_type: str = "", *, timeout: Optional[float] = None, **kwargs) -> Union[Image, RawImage]:
+        return await super().get_image(mime_type, timeout=timeout, **kwargs)
+
+    async def get_properties(self, *, timeout: Optional[float] = None, **kwargs) -> Camera.Properties:
+        return await super().get_properties(timeout=timeout, **kwargs)
 
 
 async def run(host: str, port: int, log_level: int):
@@ -35,7 +54,7 @@ async def run(host: str, port: int, log_level: int):
 if __name__ == "__main__":
     host = "localhost"
     port = 8081
-    log_level = logging.DEBUG
+    log_level = logging.ERROR
     try:
         host = sys.argv[1]
         port = int(sys.argv[2])
